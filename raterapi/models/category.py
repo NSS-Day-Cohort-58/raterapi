2fc9ae5da20a16bf1b81a47cbb08699a0c22c887
Steve <chortlehoort@gmail.com>
2022-11-01 10:17:04 -0500

    Updated docstrings

--- raterapi/models/category.py
+++ raterapi/models/category.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Category(models.Model):
-    """Database model for Games"""
+    """Database model for categories"""
     description = models.CharField(max_length=1000)
--- raterapi/models/game_review.py
+++ raterapi/models/game_review.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class GameReview(models.Model):
-    """Database model for relationship between games and categories"""
+    """Database model for a game review"""
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     game = models.ForeignKey('Game', on_delete=models.CASCADE)
     review = models.CharField(max_length=1000)
